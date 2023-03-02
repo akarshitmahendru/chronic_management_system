8214b7421b0b139c2ee647b052e102288916680a
Akarshit Mahendru <arjun.bhatt077@gmail.com>
2023-03-02 13:56:27 +0530

    changes

--- accounts/apis.py
+++ accounts/apis.py
@@ -15,8 +15,10 @@ class LoginAPI(views.APIView):
     def post(self, request, *args, **kwargs):
         serializer = self.serializers_class(data=request.data)
         if serializer.is_valid(raise_exception=True):
+            is_new_user = False
             user = User.objects.filter(phone_number=serializer.validated_data['phone_number']).first()
             if not user:
+                is_new_user = True
                 user = User.objects.create(
                     phone_number=serializer.validated_data['phone_number'],
                     role=RoleEnum.PATIENT.value
@@ -25,6 +27,7 @@ class LoginAPI(views.APIView):
             response_dict['user_id'] = user.id
             response_dict['phone_number'] = str(user.phone_number)
             response_dict['auth-token'] = user.get_jwt_token_for_user()
+            response_dict['is_new_user'] = is_new_user
             return response.Response(response_dict, status=status.HTTP_200_OK)
         else:
             return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
