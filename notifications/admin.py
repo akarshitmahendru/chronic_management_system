adbaa5017d5c638af3f4ea0bc7859d495a41ea56
Akarshit Mahendru <arjun.bhatt077@gmail.com>
2023-03-03 03:05:54 +0530

    changes

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from notifications.models import NotificationsScheduler, Notification
+
+
+@admin.register(NotificationsScheduler)
+class NotificationSchedularAdmin(admin.ModelAdmin):
+    list_display = ('patient_plan', 'scheduled_time', 'last_triggered_on')
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'patient_plan', 'action', 'notification_title')
